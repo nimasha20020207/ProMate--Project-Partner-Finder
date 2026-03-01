ee67ee1ef1a67863173b11991935191f551e0af1
nimasha20020207 <nimashakarunarathne61@gmail.com>
2026-03-01 15:49:00 +0530

    initialized frontend pages for recommendation engine module

---
+++ frontend/src/pages/RecEngine/CandidateFilter.jsx
@@ -0,0 +1,12 @@
+import React from 'react'
+import Navbar from '../../components/Navbar'
+
+const CandidateFilter = () => {
+  return (
+    <div>
+      <Navbar/>
+    </div>
+  )
+}
+
+export default CandidateFilter
