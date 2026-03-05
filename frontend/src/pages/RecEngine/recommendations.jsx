b9d60585afc095e2894d0ed6543aa9bbef5a1ce8
nimasha20020207 <nimashakarunarathne61@gmail.com>
2026-03-05 14:39:27 +0530

    created recommendations page for recommended projects

---
+++ frontend/src/pages/RecEngine/recommendations.jsx
@@ -0,0 +1,39 @@
+import React,{useEffect,useState} from 'react'
+
+const recommendations = () => {
+  const [projects,setProjects] = useState([])
+
+    useEffect(()=>{
+
+    fetch("http://localhost:5000/api/recommendations/projects/S1")
+    .then(res=>res.json())
+    .then(data=>setProjects(data))
+
+    },[])
+
+    return(
+
+    <div>
+
+    <h2>Recommended Projects</h2>
+
+    {projects.map((item,i)=>(
+
+    <div key={i} style={{border:"1px solid gray",padding:"10px",margin:"10px"}}>
+
+    <h3>{item.project.title}</h3>
+
+    <p>Score: {(item.score*100).toFixed(0)}%</p>
+
+    <p>{item.explanation}</p>
+
+    </div>
+
+    ))}
+
+    </div>
+
+    )
+}
+
+export default recommendations
