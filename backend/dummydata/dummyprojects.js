ef7c710e8e9b2df414e3e663b5344415946e0210
nimasha20020207 <nimashakarunarathne61@gmail.com>
2026-03-05 13:46:54 +0530

    created dummy data for rec engine

---
+++ backend/dummydata/dummyprojects.js
@@ -0,0 +1,65 @@
+const dummyprojects = [
+
+{
+id:"P1",
+
+title:"E-Commerce Web App",
+
+essentialSkills:{
+languages:["JavaScript"],
+frameworks:["React"],
+libraries:[],
+databases:["MongoDB"],
+tools:["Git"]
+},
+
+requiredRoles:["Frontend Developer"],
+
+availabilityRequirement:{
+weeklyHours:15
+},
+
+domain:["Web Development"],
+
+academicConstraints:{
+specialization:["SE","IT"],
+year:3,
+semester:1,
+minimumCGPA:3
+}
+
+},
+
+{
+id:"P2",
+
+title:"AI Chatbot",
+
+essentialSkills:{
+languages:["Python"],
+frameworks:[],
+libraries:["TensorFlow"],
+databases:[],
+tools:[]
+},
+
+requiredRoles:["ML Engineer"],
+
+availabilityRequirement:{
+weeklyHours:10
+},
+
+domain:["AI / ML"],
+
+academicConstraints:{
+specialization:["DS","SE"],
+year:4,
+semester:2,
+minimumCGPA:3
+}
+
+}
+
+]
+
+module.exports = dummyprojects
---
+++ backend/dummydata/dummystudents.js
@@ -0,0 +1,65 @@
+const dummystudents = [
+
+{
+id:"S1",
+
+fullName:"Alice",
+
+skills:{
+languages:[{name:"JavaScript"}],
+frameworks:[{name:"React"}],
+libraries:[],
+databases:[{name:"MongoDB"}],
+tools:[{name:"Git"}]
+},
+
+availability:{
+weeklyHours:20
+},
+
+interests:["Web Development"],
+
+preferredRoles:["Frontend Developer"],
+
+academicInfo:{
+specialization:"SE",
+year:3,
+semester:1,
+cgpa:3.5
+}
+
+},
+
+{
+id:"S2",
+
+fullName:"Bob",
+
+skills:{
+languages:[{name:"Python"}],
+frameworks:[],
+libraries:[{name:"TensorFlow"}],
+databases:[],
+tools:[]
+},
+
+availability:{
+weeklyHours:15
+},
+
+interests:["AI / ML"],
+
+preferredRoles:["ML Engineer"],
+
+academicInfo:{
+specialization:"DS",
+year:4,
+semester:2,
+cgpa:3.7
+}
+
+}
+
+]
+
+module.exports = dummystudents
