db6ef7109eef01a04e9c2cc4f6c4ae647a19377c
Pradeepa Dineshika <me.pradeepad@gmail.com>
2026-03-25 22:42:56 +0530

    BE - read
    Retrieve all posts

--- backend/routes/posts.js
+++ backend/routes/posts.js
@@ -7,10 +7,19 @@ const Posts = require("../models/post");
 //test route
 router.get("/post", (req, res) => res.send("post route testing"));
 
-//add post
+//add post - insert
 router.post("/", (req, res) => {
     Posts.create(req.body)
     .then(() => res.json({msg: "post added successfully"}))
     .catch(() => res.status(400).json({msg: "unable to add post"}));
 });
+
+//get all posts - read
+router.get("/", (req, res) => {
+    Posts.find()
+    .then((posts) => res.json(posts))
+    .catch(() => res.status(400).json({msg: "unable to fetch posts"}));
+});
+
+
 module.exports = router;
