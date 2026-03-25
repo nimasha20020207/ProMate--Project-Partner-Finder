d2629d0ffe10c664d551dde8b40c787cae8d67fd
Pradeepa Dineshika <me.pradeepad@gmail.com>
2026-03-25 17:37:27 +0530

    BE - insert part
    insert posts to DB

--- backend/routes/posts.js
+++ backend/routes/posts.js
@@ -7,5 +7,10 @@ const Posts = require("../models/post");
 //test route
 router.get("/post", (req, res) => res.send("post route testing"));
 
-
+//add post
+router.post("/", (req, res) => {
+    Posts.create(req.body)
+    .then(() => res.json({msg: "post added successfully"}))
+    .catch(() => res.status(400).json({msg: "unable to add post"}));
+});
 module.exports = router;
--- backend/server.js
+++ backend/server.js
@@ -1,12 +1,23 @@
 const express =require("express");
 const dbconnection=require("./config/db");
+const routes=require("./routes/posts");
+const bodyParser = require("body-parser");
+const cors = require("cors");
 
 const app = express();
+app.use(cors({origin:true, Credentials:true}));
+
 
 //DB connection
 dbconnection();
 
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({extended: true}));
+
+
+
 app.get("/",(req,res)=>res.send("Server is running.."));
+app.use("/api/posts",routes);
 
 const PORT=3000;
 
