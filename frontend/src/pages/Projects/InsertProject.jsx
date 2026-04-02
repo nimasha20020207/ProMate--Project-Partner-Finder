f925903d8d1375b02949aa4a84a5b3f51d2988fd
Pradeepa Dineshika <me.pradeepad@gmail.com>
2026-04-03 03:07:02 +0530

    change essential to optional
    change skills of optional

--- frontend/src/pages/Projects/InsertProject.jsx
+++ frontend/src/pages/Projects/InsertProject.jsx
@@ -372,7 +372,7 @@ const InsertProject = ({ onSuccess }) => {
               <CheckboxGroup
                 options={["Redux","Axios","jQuery","Lodash","TensorFlow","Keras","PyTorch","Scikit-learn","Pandas","NumPy","Chart.js","D3.js","Three.js","Socket.io","Bootstrap"]}
                 selectedValues={formData.essentialSkills.libraries}
-                onChange={(values) => handleSkillChange('essentialSkills', 'libraries', values)}
+                onChange={(values) => handleSkillChange('optionalSkills', 'libraries', values)}
               />
             </div>
 
