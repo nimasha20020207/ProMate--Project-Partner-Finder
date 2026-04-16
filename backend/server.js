b5eb1f1182644ee2721257abf04cec2113e9286a
Pradeepa Dineshika <me.pradeepad@gmail.com>
2026-04-16 18:58:04 +0530

    add 5174 core

--- backend/server.js
+++ backend/server.js
@@ -18,7 +18,7 @@ const adminSuspendRoutes = require("./routes/adminSuspendRoutes");
 const app = express();
 
 // Middleware
-app.use(cors({ origin: "http://localhost:5173", credentials: true }));
+app.use(cors({ origin: ["http://localhost:5173", "http://localhost:5174"], credentials: true }));
 app.use(express.json());
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true }));
